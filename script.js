695259d2fe00ece498c3be11f0d62ad463ff882a
owintanphat <firmwareios5@gmail.com>
2025-08-14 10:03:24 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function playWord() {
+    const audio = new Audio('path/to/audio/file.mp3'); // Thay đổi đường dẫn tới file âm thanh
+    audio.play();
+}
+
+function startRecording() {
+    // Logic để bắt đầu ghi âm
+}
+
+// Thêm các chức năng khác tại đây
